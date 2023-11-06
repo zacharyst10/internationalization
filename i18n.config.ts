bbea9ee7e808e699b928a409b1f0c5139826b94a
Zachary Stout <stout.zachary@gmail.com>
2023-11-05 19:57:26 -0700

    added in french

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: 'en',
-  locales: ['en', 'de', 'fi']
+  locales: ['en', 'de', 'fi', 'fr']
 } as const
 
 export type Locale = (typeof i18n)['locales'][number]
